387cab42242b6f74164042c272a13d6d62b2dfed
Roger Zhuang <zhuangziyin@gmail.com>
2022-12-26 00:18:59 -0800

    add buttons swipe event listeners

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@ let blog = document.querySelector(".blogButtons");
 let blogButtons = document.querySelectorAll(".blogButtons button");
 let twitter = document.querySelector(".twitterButtons");
 let twitterButtons = document.querySelectorAll(".twitterButtons button");
+let touchstartX = 0
+let touchendX = 0
 
 for (let btn of blogButtons) {
     btn.classList.add("buttonGrey");
@@ -37,3 +39,63 @@ twitter.addEventListener("click", (ev)=> {
     }
 })
 
+function checkDirection(buttons) {
+    if (touchendX < touchstartX) {
+        // alert('swiped left!')
+        let i = 0;
+        for (let btn of buttons) {
+            if (btn.classList.contains("buttonRed")) {
+                btn.classList.remove("buttonRed");
+                btn.classList.add("buttonGrey");
+                break;
+            };
+            i++;
+        }
+        if (i !== 2) {
+            buttons[i+1].classList.remove("buttonGrey");
+            buttons[i+1].classList.add("buttonRed");
+        }
+        else {
+            buttons[2].classList.remove("buttonGrey");
+            buttons[2].classList.add("buttonRed");
+        }
+    }   
+    if (touchendX > touchstartX) {
+        // alert('swiped right!')
+        let i = 0;
+        for (let btn of buttons) {
+            if (btn.classList.contains("buttonRed")) {
+                btn.classList.remove("buttonRed");
+                btn.classList.add("buttonGrey");
+                break;
+            };
+            i++;
+        }
+        if (i !== 0) {
+            buttons[i-1].classList.remove("buttonGrey");
+            buttons[i-1].classList.add("buttonRed");
+        }
+        else {
+            buttons[0].classList.remove("buttonGrey");
+            buttons[0].classList.add("buttonRed");
+        }
+    }
+}
+
+document.querySelector(".blogPosts").addEventListener('touchstart', (ev) => {
+    touchstartX = ev.changedTouches[0].screenX
+})
+
+document.querySelector(".blogPosts").addEventListener('touchend', (ev) => {
+    touchendX = ev.changedTouches[0].screenX
+    checkDirection(blogButtons)
+})
+
+document.querySelector(".twitterPosts").addEventListener('touchstart', (ev) => {
+    touchstartX = ev.changedTouches[0].screenX
+})
+
+document.querySelector(".twitterPosts").addEventListener('touchend', (ev) => {
+    touchendX = ev.changedTouches[0].screenX
+    checkDirection(twitterButtons)
+})
